3b352d9c6137e153ebc4ccda1f0cc91fa34864c0
chiefkufre <samuelkufrewillie@gmail.com>
2022-10-11 11:55:24 +0100

    chore: add default auth and permission classes to setting

--- drf/backend/cfehome/settings.py
+++ drf/backend/cfehome/settings.py
@@ -128,5 +128,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'api.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': [
+
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+
+    ]
 }
